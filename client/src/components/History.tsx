9c518f5ac946443b5a66fa50b4572372fcb4f865
Zonotora <Zonotora@hotmail.com>
2021-11-01 19:59:07 +0100

    Rework moves panel

--- client/src/components/History.tsx
+++ client/src/components/History.tsx
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface IHistory {
   moves: string[];
   onClick: (move: number) => void;
 }
 
-const History: React.FC<IHistory> = ({ moves, onClick }) => {
+interface IRow {
+  index: number;
+  white: string;
+  black: string;
+  onClickWhite: () => void;
+  onClickBlack: () => void;
+}
+
+const Row: React.FC<IRow> = ({
+  index,
+  white,
+  black,
+  onClickWhite,
+  onClickBlack,
+}) => {
   return (
-    <>
-      {moves.map((move, i) => (
-        <div className="move" onClick={() => onClick(i)}>
-          {move}
-        </div>
-      ))}
-    </>
+    <div className="row">
+      <div className="row-index"> {index}</div>
+      <div className="row-white" onClick={onClickWhite}>
+        {white}
+      </div>
+      <div className="row-black" onClick={onClickBlack}>
+        {black}
+      </div>
+    </div>
   );
+};
+
+const History: React.FC<IHistory> = ({ moves, onClick }) => {
+  const [rows, setRows] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    const tRows = [];
+    for (let i = 0; i < moves.length; i += 2) {
+      tRows.push(
+        <Row
+          index={i / 2 + 1}
+          white={moves[i]}
+          black={i < moves.length ? moves[i + 1] : ""}
+          onClickWhite={() => onClick(i)}
+          onClickBlack={i < moves.length ? () => onClick(i + 1) : () => {}}
+        />
+      );
+    }
+    setRows(tRows);
+  }, [moves]);
+
+  return <>{rows}</>;
 };
 
 export default History;
