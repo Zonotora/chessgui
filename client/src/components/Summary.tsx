10b204ee4dac5c625ebb3587a2501b41066c4748
Zonotora <Zonotora@hotmail.com>
2021-11-02 16:21:39 +0100

    Update styling for summary

--- client/src/components/Summary.tsx
+++ client/src/components/Summary.tsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
 
-const colors = ["green", "yellow", "red", "orange"];
+const colors = [
+  "#3deccf",
+  "#7ae24a",
+  "#b4e24a",
+  "#b3d487",
+  "#b39f7a",
+  "#f0d29b",
+  "#f8bc4c",
+  "#f8664c",
+];
 
 interface IPoint {
   x: number;
@@ -46,42 +55,68 @@ const Summary: React.FC = () => {
             markerWidth="4"
             markerHeight="4"
             orient="auto-start-reverse"
+            fill="white"
           >
             <path d="M 0 0 L 10 5 L 0 10 z" />
           </marker>
-        </defs>
-        <line
-          x1="50"
-          y1="10"
-          x2="50"
-          y2="90"
-          stroke="black"
-          strokeWidth="0.5"
-          markerEnd="url(#arrow)"
-          markerStart="url(#arrow)"
-        />
 
-        {[20, 30, 40, 50, 60, 70, 80].map((y) => (
-          <line
-            x1="20"
-            y1={y}
-            x2="80"
-            y2={y}
-            stroke="#202020"
-            strokeWidth="0.5"
-          />
+          <clipPath id="round-corner-top">
+            <rect x="20" y="10" width="60" height="13" rx="3" ry="3" />
+          </clipPath>
+          <clipPath id="round-corner-bottom">
+            <rect x="20" y="77" width="60" height="13" rx="3" ry="3" />
+          </clipPath>
+        </defs>
+
+        {[
+          { y: 10, c: "#3deccf", clipPath: "url(#round-corner-top)" },
+          { y: 20, c: "#7ae24a" },
+          { y: 30, c: "#b4e24a" },
+          { y: 40, c: "#b3d487" },
+          { y: 50, c: "#b39f7a" },
+          { y: 60, c: "#f0d29b" },
+          { y: 70, c: "#f8bc4c" },
+          { y: 80, c: "#f8664c", clipPath: "url(#round-corner-bottom)" },
+        ].map(({ y, c, clipPath }) => (
+          <>
+            <rect
+              x="20"
+              y={y}
+              width="60"
+              height="10"
+              fill={`${c}3f`}
+              strokeWidth="0"
+              clipPath={clipPath}
+            />
+            <text text-anchor="end" x="15" y={y + 7} fill={c}>
+              {0}
+            </text>
+            <text x="85" y={y + 7} fill={c}>
+              {0}
+            </text>
+          </>
         ))}
 
         {points?.map((p) => (
           <circle
             cx={p.x}
             cy={p.y}
-            r="1"
-            fill="none"
-            stroke={p.c}
-            strokeWidth="0.5"
+            r="1.2"
+            fill={p.c}
+            style={{ cursor: "pointer" }}
           />
         ))}
+
+        <line
+          x1="50"
+          y1="8"
+          x2="50"
+          y2="92"
+          stroke="white"
+          strokeWidth="0.5"
+          markerEnd="url(#arrow)"
+          markerStart="url(#arrow)"
+        />
       </svg>
     </div>
   );
